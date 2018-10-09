059a80d5163644bfbf9e005aaf4104f4af6df700
NicolasG <nicolas.gianni@wolox.com.ar>
2018-10-09 15:24:31 -0300

    Crear_las-operaciones_CRUD_de-_Book_Card

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,16 +1,52 @@
 package wolox.training.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import wolox.training.models.Book;
+import wolox.training.repositories.BookRepository;
+
 
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+
+    @GetMapping("/{id}")
+    public Book findOne(@PathVariable Long id ) {
+        return bookRepository.findOnById(id);
+    }
+
+    public Book findOne(@PathVariable String author ) {
+        return bookRepository.findOneByAuthor(author);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book create(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookRepository.findOnById(id);
+        bookRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
+        bookRepository.findOnById(id);
+        return bookRepository.save(book);
+    }
+
 }
---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package wolox.training.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import wolox.training.models.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    Book findOnById(Long id);
+    Book findOneByAuthor(String author);
+
+}
