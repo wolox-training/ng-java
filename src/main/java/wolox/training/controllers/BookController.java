6bbd1536cb1b63769e26ea96293bc3dc1b5279dc
NicolasG <nicolas.gianni@wolox.com.ar>
2018-10-09 18:54:48 -0300

    Corrections 2: Crear_las-operaciones_CRUD_de-_Book_Card

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -5,13 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import wolox.training.exception.BookIdMismatchException;
-import wolox.training.exception.BookNotFoundException;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
-
-import java.util.Optional;
-
 
 @Controller
 public class BookController {
@@ -28,7 +23,7 @@ public class BookController {
     @GetMapping("/{id}")
     public Book findOne(@PathVariable Long id) {
         return bookRepository.findById(id)
-                .orElseThrow(BookNotFoundException::new);
+                .orElseThrow(RuntimeException::new);
     }
 
     @PostMapping
@@ -40,17 +35,17 @@ public class BookController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         bookRepository.findById(id)
-                .orElseThrow(BookNotFoundException::new);
+                .orElseThrow(RuntimeException::new);
         bookRepository.deleteById(id);
     }
 
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
         if (book.getId() != id) {
-            throw new BookIdMismatchException();
+            throw new RuntimeException();
         }
         bookRepository.findById(id)
-                .orElseThrow(BookNotFoundException::new);
+                .orElseThrow(RuntimeException::new);
         return bookRepository.save(book);
     }
 
