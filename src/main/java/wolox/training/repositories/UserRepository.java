3a8333043e7ec47b7f8f73ed93bd117aeae3ae50
NicolasG <nicolas.gianni@wolox.com.ar>
2018-10-10 16:48:15 -0300

    Crear_un_Repositorio_de_Users_Card

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package wolox.training.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import wolox.training.models.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findOneByUserName(String userName);
+}
